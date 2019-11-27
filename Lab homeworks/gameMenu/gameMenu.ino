944555f25a7620dfe033dfcf2ffd950bae7be6b2
ToniBiro <33375662+ToniBiro@users.noreply.github.com>
2019-11-28 01:12:37 +0200

    Update gameMenu.ino

--- Lab homeworks/gameMenu/gameMenu.ino
+++ Lab homeworks/gameMenu/gameMenu.ino
@@ -1,4 +1,7 @@
-# include<LiquidCrystal.h>
+# include <LiquidCrystal.h>
+# include <EEPROM.h>
+# include <string.h>
+int addr = 0;
 const int RS = 12;
 const int enable = 11;
 const int d4 = 5;
@@ -6,27 +9,82 @@ const int d5 = 4;
 const int d6 = 3;
 const int d7 = 2;
 
-const int swPin = A2;
+const int swPin = 8;
 const int xPin = A0;
 const int yPin = A1;
 
-unsigned long lastPrint = 0;
-
 LiquidCrystal lcd(RS, enable, d4, d5, d6, d7);
+
+void print_main_menu(){
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print("Start");
+  lcd.setCursor(8, 0);
+  lcd.print("Settings");
+  lcd.setCursor(1, 1);
+  lcd.print("HighScore");
+}
+
+void print_start_menu(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Lives:");
+  lcd.setCursor(8, 0);
+  lcd.print("Level:");
+  lcd.setCursor(0, 1);
+  lcd.print("Score:");
+}
+
+void print_game_over(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Game Over!");
+  delay(2000);
+}
+
+void print_highScore(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("HighScore:");
+  lcd.setCursor(0, 1);
+  lcd.print("Name:");
+}
+
+void print_settings(){
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print("Choose level:");
+  lcd.setCursor(1, 1);
+  lcd.print("Name:");
+}
+
 void setup() {
-  Serial.begin(9600); // Starts the serial communication
+  Serial.begin(9600);
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
-  lcd.print("Hello World");
+  lcd.print("Menu Homework:)!");
+  delay(2500);
+  print_main_menu();
+  
   pinMode(xPin, INPUT);
   pinMode(yPin, INPUT);
   pinMode(swPin, INPUT_PULLUP);
 }
 
-unsigned long lastDebounceTime;
-int debounceDelay = 500;
-int buttonState,lastButtonState;
+unsigned long lastPrint = 0;
+unsigned long lastDebounceTime = 0;
+int debounceDelay = 50;
+int buttonState= 0;
+int lastButtonState = 0;
+
+const int minThreshold = 400;
+const int maxThreshold = 600;
+int xValue;
+int yValue;
 int swState;
+bool joyMoved = false;
+int cursorPosition = 0;
+int settingsCursor = 1;
 
 void debounce(bool reading){
   if (reading != lastButtonState) {
@@ -42,27 +100,192 @@ void debounce(bool reading){
       }
     }
   }
+  lastButtonState = reading;
 }
 
-int xRead, yRead, swRead;
+int highScore = 0;
+int score = 0;
+int level = 1;
+int lives = 3;
+unsigned long initEntryTime;
+unsigned long entryTime;
+char player[10]; 
+char inChar; 
+byte index = 0;
+
+bool check = true;
 
 void loop() {
 
-  xRead = analogRead(xPin);
-  yRead = analogRead(yPin);
-  swRead = digitalRead(swPin);
-  debounce(swRead);
+  xValue = analogRead(xPin);
+  yValue = analogRead(yPin);
+  debounce(digitalRead(swPin));
 
   Serial.println(swState);
-    
-  if(millis() - lastPrint >=500){
-    lastPrint = millis();
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print(xRead);
-    lcd.print(" ");
-    lcd.print(yRead);
-    lcd.print(" ");
-    lcd.print(swState);
+  
+  if (yValue > maxThreshold && joyMoved == false && swState == 1) {
+    if(cursorPosition >= 3)
+      cursorPosition = 1;
+    else
+      cursorPosition++;
+    print_main_menu();
+    if(cursorPosition == 1){
+      lcd.setCursor(0, 0);
+      lcd.print(">");
+    }
+    else{
+      if(cursorPosition == 2){
+        lcd.setCursor(7, 0);
+        lcd.print(">");
+      }
+      else{
+        lcd.setCursor(0, 1);
+        lcd.print(">");
+      }
+    }
+    joyMoved = true;
   }
+
+  if(cursorPosition == 1 && swState == 0){
+    if(check == true){
+      initEntryTime = millis();
+      entryTime = millis();
+      print_start_menu();
+      check = false;
+      score = 0;
+    }
+    if(millis() - initEntryTime >= 10000){
+      print_game_over();
+      print_main_menu();
+      swState = 1;
+      check = true;
+    }
+    else{
+      // we are in the START menu
+      lcd.setCursor(6, 0);
+      lcd.print(lives);
+      lcd.setCursor(14, 0);
+      lcd.print(level);
+      lcd.setCursor(7, 1);
+      lcd.print(score);
+      if(millis() - entryTime >= 3000){
+        score = score + level * 3;
+        entryTime = millis();
+      }   
+    }
+   if(score > highScore){
+    highScore = score;
+    addr = 0;
+    EEPROM.write(addr, highScore);
+    addr++;   
+   }
+  }
+
+  if(cursorPosition == 2 && swState == 0){
+    if(check == true){
+      print_settings();
+      lcd.setCursor(0, 0);
+      lcd.print(">");
+      check = false;
+    }
+    if(yValue > maxThreshold && joyMoved == false){
+      if(settingsCursor == 1){
+        settingsCursor = 2;
+        lcd.setCursor(0, 0);
+        lcd.print(" ");
+        lcd.setCursor(0, 1);
+        lcd.print(">");
+      }
+      else{
+          if(settingsCursor == 2){
+            settingsCursor = 1;
+            lcd.setCursor(0, 1);
+            lcd.print(" ");
+            lcd.setCursor(0, 0);
+            lcd.print(">");
+          }
+      }
+      joyMoved = true;
+    }
+    if(xValue > maxThreshold && joyMoved == false && settingsCursor == 1){
+      if(level >= 10){
+        level = 10;
+      }
+      else{
+        level++;
+      }
+      joyMoved = true;
+    }
+    if(xValue < minThreshold && joyMoved == false && settingsCursor == 1){
+      if(level <= 1){
+        level = 1;
+      }
+      else{
+        level--;
+      }
+      joyMoved = true;
+    }
+    lcd.setCursor(14, 0);
+    lcd.print(level);
+
+    if(settingsCursor == 2){
+      if(Serial.available() > 0){
+        
+        if(index < 19) // One less than the size of the array
+        {
+          inChar = Serial.read(); // Read a character
+          player[index] = inChar; // Store it
+          index++; // Increment where to write next
+          player[index] = ' ';
+        }
+        
+        lcd.setCursor(6, 1);
+        lcd.print(player);
+        addr = 2;
+        EEPROM.write(addr, strlen(player));
+        addr++;
+        int i;
+        for(int i = 0; i < strlen(player); ++i){
+          EEPROM.write(addr, player[i]);
+          addr++;
+        }
+      }
+    }
+  }
+
+  if(cursorPosition == 3 && swState == 0){
+    int hs;
+    char name_player[10];
+    if(check == true){
+      print_highScore();
+      int aux_addr = 0;
+      hs = EEPROM.read(aux_addr);
+      aux_addr++;
+      int i, n = EEPROM.read(aux_addr);
+      aux_addr++;
+      for(i = 0; i < n; ++i){
+        name_player[i] = EEPROM.read(aux_addr);
+        aux_addr++;
+      }
+      check = false;
+    }
+    
+    
+    lcd.setCursor(10, 0);
+    lcd.print(hs);
+    lcd.setCursor(5, 1);
+    lcd.print(name_player);
+  }
+  
+  if (xValue >= minThreshold && xValue <= maxThreshold && yValue >= minThreshold && yValue <= maxThreshold) {
+    joyMoved= false;
+  }
+  
+  
+  
+   if(swState == 1 && check == false){
+    print_main_menu();
+    check = true;
+   }
+   
 }
